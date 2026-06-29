21acf4f154b6333ce841dcebff14a4cf3f24f76c
jero <jeronatan@hotmail.com>
2026-06-29 18:54:09 -0300

    Test VERDE: Ahorcado arranca con 6 vidas

--- src/domain/Ahorcado.ts
+++ src/domain/Ahorcado.ts
@@ -11,4 +11,8 @@ export class Ahorcado {
       .map(() => "_")
       .join(" ");
   }
+
+  vidas(): number {
+    return 6; // En el AT #1 ponemos que desvuelva 6 siempre, ya que aún no hicimos la funcionalidad de que bajen las vidas.
+  }
 }
