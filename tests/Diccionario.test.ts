8cb6291c9389d9bd152bc2c931fa60670b1277b9
jero <jeronatan@hotmail.com>
2026-07-07 22:53:50 -0300

    Test ROJO: Diccionario da una palabra dificil de 6 letras

--- tests/Diccionario.test.ts
+++ tests/Diccionario.test.ts
@@ -13,4 +13,10 @@ describe("Diccionario", () => {
     const palabra = diccionario.palabraAlAzar("normal");
     expect(palabra.length).toBe(5);
   });
+
+   it("una palabra dificil tiene 6 letras", () => {
+    const diccionario = new Diccionario();
+    const palabra = diccionario.palabraAlAzar("dificil");
+    expect(palabra.length).toBe(6);
+  });
 });
